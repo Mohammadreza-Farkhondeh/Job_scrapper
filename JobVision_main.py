cde2f920cf67a769f2d46671552d0b525a4a070b
Mohammadrza Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-04-22 02:01:25 +0330

    Final

--- JobVision_main.py
+++ JobVision_main.py
@@ -11,19 +11,17 @@ driver = webdriver.Chrome()
 desired_links = []
 
 # Get jobs in pages first to 30th
-for p in range(1, 2):
+for p in range(1, 30):
     # Get page url to driver and make soup
     page_url = main_url + str(p)
     driver.get(page_url)
-    # time.sleep(3)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     # Get all links in the page and filter links to job pages,
-    # append job links in the page to the full desired links
+    # Append job links in the page to the full desired links
     all_links = list(soup.find_all('a'))
     for link_j in soup.select("a[id*='el-']"):
         desired_links.append(link_j['id'].split('-')[1])
-
 
 # Define skills list
 skills = []
@@ -57,7 +55,6 @@ sorted_list = sorted(DATA.items(), key=lambda item: int(item[1]), reverse=True)
 print(sorted_list)
 for i in sorted_list:
     Sorted_DATA.update({i[0]: i[1]})
-
 
 # Export DATA to excel file
 pandas.DataFrame(Sorted_DATA, index=[0]).T.to_csv('JOB_DATA.csv')
