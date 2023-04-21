52be1f5879a7ed9c3dc9a3b263fcfdde7d12602f
Mohammadrza Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-04-22 00:06:11 +0330

    maked script of scrap 30 pages for getting 730 job page

--- JobVision_main.py
+++ JobVision_main.py
@@ -9,12 +9,19 @@ from selenium import webdriver
 main_url = 'https://jobvision.ir/jobs/category/developer?page='
 driver = webdriver.Chrome()
 
+# Define desired links list of links to jobs
+desired_links = []
+
 # Get jobs in pages first to 30th
 for p in range(1, 30):
     # Get page url to driver and make soup
     page_url = main_url + str(p)
     driver.get(page_url)
-    time.sleep(5)
+    # time.sleep(3)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-
+    # Get all links in the page and filter links to job pages,
+    # append job links in the page to the full desired links
+    all_links = list(soup.find_all('a'))
+    for link_j in soup.select("a[id*='el-']"):
+        desired_links.append(link_j['id'].split('-')[1])
