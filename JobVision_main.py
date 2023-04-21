a7b25be4cdc0e2e48ee8434228e24b2df0de16d3
Mohammadrza Farkhondeh <mohammr.farkhonde12@sharif.edu>
2023-04-21 23:47:46 +0330

    Initialized and defined driver

--- JobVision_main.py
+++ JobVision_main.py
@@ -8,3 +8,13 @@ from selenium import webdriver
 # Define main url and driver
 main_url = 'https://jobvision.ir/jobs/category/developer?page='
 driver = webdriver.Chrome()
+
+# Get jobs in pages first to 30th
+for p in range(1, 30):
+    # Get page url to driver and make soup
+    page_url = main_url + str(p)
+    driver.get(page_url)
+    time.sleep(5)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
