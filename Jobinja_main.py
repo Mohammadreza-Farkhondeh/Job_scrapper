045b0dcdb28de016c4bb2c07ee1887fc7a9a7140
Mohammadreza-Farkhondeh <mohammr.farkhondeh@sharif.edu>
2024-02-21 23:32:17 +0330

    update for data engineering

--- Jobinja_main.py
+++ Jobinja_main.py
@@ -1,4 +1,3 @@
-# Import libraries
 from bs4 import BeautifulSoup
 from nltk import FreqDist
 from nltk.corpus import stopwords
@@ -10,67 +9,41 @@ import my_word_cloud
 import pandas
 
 
-# Define the Page class
 class Page:
-
-    # Define the constructor
     def __init__(self, url):
-        # Assign the url argument to an instance attribute
         self.url = url
 
-    # Define the scrape method
     def scrape(self):
-        # Change user agent to firefox because python-requests is blocked by jobinja
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
-        # Get the page by request
         page = requests.get(self.url, headers=headers)
-        # Get the HTML page source
         html = page.text
-        # Parse the HTML content using Beautiful Soup
         soup = BeautifulSoup(html, "html.parser")
-        # Return the soup object
         return soup
 
-
-# Define the JobPage class that inherits from Page
 class JobPage(Page):
 
-    # Define the constructor
     def __init__(self, url):
-        # Call the parent constructor with the url argument
         super().__init__(url)
-        # Create an empty Text object to store the text extracted from the web page
         self.text = Text()
 
-    # Define the extract_text method
     def extract_text(self):
-        # Call the parent scrape method and get the soup object
         soup = self.scrape()
-        # Extract the text from the <div> element with class="..." this class is for persian texts
-        # Make sure to not fail in first try
         div = soup.find("div", class_="o-box__text s-jobDesc c-pr40p")
-        # checking the div to not be None(if the content is english, class is differnt)
         if div:
             pass
         else:
-            # finding new div
             div = soup.find("div", class_="o-box__text s-jobDesc u-ltr c-pl40p")
 
         text = div.get_text()
-        # Assign the text to the Text object's content attribute
         self.text.content = text
 
-    # Define the get skill method
     def get_skill_tags(self):
-        # Call the parent scrape method and get the soup object
         soup = self.scrape()
-        # Find all li item with specified class to find skill spans not gender and diploma
         lis = soup.find_all('li', {'class': "c-infoBox__item"})
         for li in lis:
             if li.find('h4').text == 'مهارت‌های مورد نیاز':  # If li was about skills, then find spans inside
                 spans = li.find_all('span', {'class': 'black'})
 
-        # Define skills list and append found spans to it
         skills = []
         for span in spans:
             skills.append(span.text)
@@ -78,47 +51,29 @@ class JobPage(Page):
         return skills
 
 
-# Define the JobListPage class that inherits from Page
 class JobListPage(Page):
 
-    # Define the constructor
     def __init__(self, url):
-        # Call the parent constructor with the url argument
         super().__init__(url)
-        # Create an empty list to store the JobPage objects for each job link
         self.jobs = []
 
-    # Define the extract_links method
     def extract_links(self):
-        # Call the parent scrape method and get the soup object
         soup = self.scrape()
-        # Find all the <a> elements with class="jobTitle"
         links = soup.find_all("a", {"class": "c-jobListView__titleLink"})
-        # Loop through each link element
         for link in links:
-            # Get the href attribute of the <a> element
             href = link.get("href")
-            # Create a JobPage object with the full URL as an argument
             job_page = JobPage(href)
-            # Append the JobPage object to the jobs list
             self.jobs.append(job_page)
 
 
-# Define the Text class that represents the text extracted from a web page
 class Text:
 
-    # Define the constructor
     def __init__(self):
-        # Create an empty string to store the text content
         self.content = ""
-        # Create an empty Counter object to store the word frequencies
         self.freq = Counter()
 
-    # Define the process_text method
     def process_text(self, method):
-        # Tokenize the content into words using NLTK
         words = word_tokenize(self.content)
-        # Remove stopwords, punctuation, and numbers from the words
         stop_words = ['كسي', 'ريزي', 'رفت', 'گردد', 'مثل', 'آمد', 'ام', 'بهترين', 'دانست', 'كمتر', 'دادن', 'تمامي',
                       'جلوگيري', 'بيشتري', 'ايم', 'ناشي', 'چيزي', 'آنكه', 'بالا', 'بنابراين', 'ايشان', 'بعضي', 'دادند',
                       'داشتند', 'برخوردار', 'نخواهد', 'هنگام', 'نبايد', 'غير', 'نبود', 'ديده', 'وگو', 'داريم', 'چگونه',
@@ -152,31 +107,24 @@ class Text:
                         list(string.punctuation) + ["’", "“", "”"]
         words = [str(word.lower()) for word in words if word.lower() not in stop_words and word.isalpha()]
 
-        # There is two method one get you number of repetitions, one get only existence of words
         if method == 1:
-            # Calculate the frequency of each word using NLTK FreqDist and assign it to the freq attribute
             self.freq = FreqDist(words)
 
         elif method == 2:
-            # Create an empty set and an empty dictionary
             seen = set()
             self.freq = Counter()
-            # Loop through the words and assign a frequency of 1 to the new words
             for word in words:
                 if word not in seen:
                     seen.add(word)
                     self.freq[word] = 1
 
-        # Update the global_freq with the local freq
         global_freq.update(self.freq)
 
-    # Define the get_keywords method that returns the top n most frequent words as keywords
     def get_keywords(self, n):
         return self.freq.most_common(n)
 
 
 def get_tags_to_DATA(skills):
-    # Append skills number to DATA
     for skill in skills:
         if skill in DATA:
             DATA[skill] += 1
@@ -185,44 +133,39 @@ def get_tags_to_DATA(skills):
 
 
 def export_tags():
-    # Sort Data
     Sorted_DATA = {}
     sorted_list = sorted(DATA.items(), key=lambda item: int(item[1]), reverse=True)
     for i in sorted_list:
         Sorted_DATA.update({i[0]: i[1]})
 
-    # Export DATA to excel file
     pandas.DataFrame(Sorted_DATA, index=[0]).T.to_csv('JOB_DATA_Jobinja.csv')
 
 
 def get_most_common_freq(n):
-    if n == 'all':
+    if n == '*':
         return global_freq
-    elif type(n) is int:
+    elif isinstance(n, int):
         return global_freq.most_common(n)
 
 
 def main():
-    JobListPage_URL = 'https://jobinja.ir/jobs/category/it-software-web-development-jobs/' \
-                      '%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85-%D9%88%D8%A8-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-' \
-                      '%D9%86%D9%88%DB%8C%D8%B3-%D9%86%D8%B1%D9%85-%D8%A7%D9%81%D8%B2%D8%A7%D8%B1?&page='
+    JobListPage_URL = 'https://jobinja.ir/jobs?filters%5Bkeywords%5D%5B0%5D=data+engineering&filters%5Blocations%5D%5B0%5D=&filters%5Bjob_categories%5D%5B0%5D=&b=&page='
     global DATA
     DATA = {}
     page = 1
     global global_freq
     global_freq = Counter()
 
-    try:  # Get the  pages and frequencies
-        for i in range(1, 199):
+    try:
+        for i in range(1, 16):
             try:
-                URL = JobListPage_URL + str(i)  # Add page number to URL
+                URL = JobListPage_URL + str(i)
                 job_list_page = JobListPage(URL)
                 job_list_page.extract_links()
             except Exception as error:
-                # handle the error
                 print(error)
 
-            for job_page in job_list_page.jobs:  # Get the words and frequencies in job_page
+            for job_page in job_list_page.jobs:
                 try:
                     job_page.extract_text()
                     job_page.text.process_text(2)
@@ -231,18 +174,14 @@ def main():
                     print(page)
                     page += 1
                 except Exception as error:
-                    # handle the error
                     print(error)
 
     except Exception as error:
-        # handle the error
         print(error)
 
 
 def get_word_clouds():
-    # Get the all words
-    word_dict = dict(get_most_common_freq('all'))
-    # Filter english words and persians
+    word_dict = dict(get_most_common_freq('*'))
     eng_words_dict, persian_words_dict = my_word_cloud.filter_eng_fa(word_dict)
     my_word_cloud.draw_fa_en_word_cloud(persian_words_dict)
     my_word_cloud.draw_fa_en_word_cloud(eng_words_dict)
